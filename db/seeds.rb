92630398005138bac2f1f6baf88c485ced62162d
sinivaal <nutt.raimondhendrik@gmail.com>
2018-09-28 15:01:43 +0300

    seeds.db file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,45 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#Adding multiple categories
+Category.create([{name: 'Muusika'}, {name: 'Teater' }, {name: 'Sport' }, {name: 'EV100' }, {name: 'Film' }, {name: 'Kogupere' }, {name: 'Kinkekaardid' }, {name: 'Varia' }, {name: 'Näitus' }])
+
+#Adding one event
+Event.create({
+   name: "Lorem ipsum",
+   short_description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+   long_description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+   price: 3000,
+   date: Date.today + 3.weeks,
+   location: "Tartu",
+   category: Category.where(name: "Muusika").first
+})
+Event.create({
+   name: "Lorem ipsum2",
+   short_description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+   long_description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+   price: 3001,
+   date: Date.today + 3.weeks + 2.days,
+   location: "Tartu",
+   category: Category.where(name: "Muusika").first
+})
+Event.create({
+   name: "Lorem ipsum3",
+   short_description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+   long_description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+   price: 3001,
+   date: Date.today - 1.year,
+   location: "Tartu",
+   category: Category.where(name: "EV100").first
+})
+Event.create({
+   name: "Lorem ipsum2",
+   short_description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+   long_description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+   price: 3001,
+   date: Date.new(2019,9,14),
+   location: "Tartu",
+   category: Category.where(name: "Muusika").first
+})
+
+User.create({email: 'm2@m.ee', password: '123123'}).confirm
+
+
+
