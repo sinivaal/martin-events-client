07db6f340caeaedebabbde8baf02adc9bafe55dd
VSo17 <jurgen.hallast@khk.ee>
2018-10-05 00:51:57 +0300

    created new models

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :event_categories
+  resources :categories
   root 'home#index'
   resources :events
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
