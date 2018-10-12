38b3255e84a642fb092ec08ab57a684d1f09c9f0
sinivaal <nutt.raimondhendrik@gmail.com>
2018-10-12 13:27:21 +0300

    root=home

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :event_categories
   resources :categories
-  get 'home/index'
+  root 'home#index'
   resources :events
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
