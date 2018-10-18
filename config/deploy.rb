edbebd4473190af0501418983a61b0eeef150ac4
sinivaal <nutt.raimondhendrik@gmail.com>
2018-10-18 12:07:44 +0300

    adding master.key to server

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,7 +21,7 @@ set :repo_url, "git@github.com:sinivaal/martin-events-client.git"
 # set :pty, true
 
 # Default value for :linked_files is []
-# append :linked_files, "config/database.yml"
+ append :linked_files, "config/master.key"
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
