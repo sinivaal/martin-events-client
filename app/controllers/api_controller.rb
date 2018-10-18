74b6dbc77932b9be9e14da6d224adb1c9c4f3a4c
sinivaal <nutt.raimondhendrik@gmail.com>
2018-10-18 14:48:28 +0300

    Adding API-about

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -2,7 +2,7 @@ class ApiController < ApplicationController
     	skip_before_action :authenticate_user!
   def about
   	render json:{
-  		name: "test",
+  		name: "Raimond",
   		time: Time.now.strftime('%H:%M'),
   		date: Time.now.strftime('%Y-%m-%d'),
   		fruits: ["banaan", "apelsin", "arbuus"]
