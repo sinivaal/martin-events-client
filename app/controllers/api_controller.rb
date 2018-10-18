80600d792af2bc7e2dad0258a49354983b1b4e38
sinivaal <nutt.raimondhendrik@gmail.com>
2018-10-18 15:25:57 +0300

    API-update update

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,11 +1,11 @@
 class ApiController < ApplicationController
     	skip_before_action :authenticate_user!
   def about
+
   	render json:{
   		name: "Raimond",
-  		time: Time.now.strftime('%H:%M'),
+  		time: Time.zone.now.strftime('%H:%M'),
   		date: Time.now.strftime('%Y-%m-%d'),
-  		time2: Time.now.getlocal('-08:00'),
   		fruits: ["banaan", "apelsin", "arbuus"]
 
   	}
